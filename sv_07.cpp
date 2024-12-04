5738d1dcc90ffd63c16391e0d8778a68c3775ce3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-04 23:22:06 +0300

    Update and rename members/members_02.cpp to sv_07.cpp

--- sv_07.cpp
+++ sv_07.cpp
@@ -7,12 +7,12 @@ int main()
 
 	std::cout << std::boolalpha;
 
-	std::cout << sw.length() << "\n"; //29
+	std::cout << sw.length() << '\n'; //29
 	std::cout << sw.substr(0, sw.find(' ')) << '\n'; // The
-	std::cout << (sw == "The poor") << "\n"; //false
+	std::cout << (sw == "The poor") << '\n'; //false
 
-	std::cout << sw.starts_with("The") << "\n"; //true
-	std::cout << sw.ends_with("pretty") << "\n";  //false
+	std::cout << sw.starts_with("The") << '\n'; //true
+	std::cout << sw.ends_with("pretty") << '\n';  //false
 
 	std::cout << "[" << sw << "]";
 }
