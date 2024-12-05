71e95e8dbb9d680f770eb7f0b54f8bff4041efb8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-05 08:05:18 +0300

    Update sv_02.cpp

--- sv_02.cpp
+++ sv_02.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-	std::string_view sw{ "The good the bad and the ugly" };
+	std::string_view sv{ "The good the bad and the ugly" };
 
 	std::cout << std::boolalpha;
 
-	std::cout << sw.length() << '\n'; //29
-	std::cout << sw.substr(0, sw.find(' ')) << '\n'; // The
-	std::cout << (sw == "The poor") << '\n'; //false
+	std::cout << sv.length() << '\n'; //29
+	std::cout << sv.substr(0, sv.find(' ')) << '\n'; // The
+	std::cout << (sv == "The poor") << '\n'; //false
 
-	std::cout << sw.starts_with("The") << '\n'; //true
-	std::cout << sw.ends_with("pretty") << '\n';  //false
+	std::cout << sv.starts_with("The") << '\n'; //true
+	std::cout << sv.ends_with("pretty") << '\n';  //false
 
-	std::cout << "[" << sw << "]";
+	std::cout << "[" << sv << "]";
 }
