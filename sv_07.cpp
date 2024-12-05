d166731b2dd45638eb17718d82156426182d4f74
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-05 08:37:28 +0300

    Update sv_07.cpp

--- sv_07.cpp
+++ sv_07.cpp
@@ -4,13 +4,11 @@
 
 int main()
 {
-	using namespace std;
+	std::string str{ "necati ergin" };
+	std::vector<char> cvec{ 's', 't', 'r', 'o', 'u', 's', 't', 'r', 'u', 'p' };
 
-	string str{ "necati ergin" };
-	vector<char> cvec{ 's', 't', 'r', 'o', 'u', 's', 't', 'r', 'u', 'p' };
-
-	string_view sv1{ str.begin(), str.end() }; //C++20
-	cout << sv1 << '\n';
-	string_view sv2{ cvec.begin(), cvec.end() }; //C++20
+	std::string_view sv1{ str.begin(), str.end() }; //C++20
+	std::cout << sv1 << '\n';
+	std::string_view sv2{ cvec.begin(), cvec.end() }; //C++20
 	cout << sv2 << '\n';
 }
